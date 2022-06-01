e452069e0d9450855840e13b732efb98ba9e449f
Somnath Thorat <thorat.somnath13@gmail.com>
2022-06-01 16:31:13 +0530

    updated on 1st june

---
+++ frontend/src/components/Navbar/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'About Us',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'contact Us',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sign In',
+        url: '#',
+        cName: 'nav-links-mobile'
+    },
+]
